861d64edf016ed1930f130adbcf2fb70af9dd2e6
xiepengyuan <1102736593@qq.com>
2024-07-23 19:45:25 +0800

    Initial commit

---
+++ ruoyi-admin/src/main/java/com/ruoyi/TestService.java
@@ -0,0 +1,10 @@
+package com.ruoyi;
+
+import com.ruoyi.test.TestSer;
+
+public class TestService {
+    public static void main(String[] args) {
+        TestSer.helloTest();
+        System.out.println(TestSer.helloTest());
+    }
+}
---
+++ rwzxgl/src/main/java/com/ruoyi/test/TestSer.java
@@ -0,0 +1,8 @@
+package com.ruoyi.test;
+
+public class TestSer {
+    public static String helloTest()
+    {
+        return "hello";
+    }
+}
